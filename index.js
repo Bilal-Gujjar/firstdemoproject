6efe5b22e07e8c292b87597c19fa08af0569f198
Bilal Gujjar <bilaltahir515@gmail.com>
2020-06-05 11:54:40 +0500

    Html file
    Golden file

---
+++ index.js
@@ -0,0 +1,14 @@
+//alert("Hellow world")
+//alert("Peak time is running please wait for 5 s")
+var a;
+a = 36;
+console.log(a+20)
+var age = 21; 
+var name = "Hello world";    
+var isFeepaid = false;
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isFeepaid);
+isFeepaid = 70;
+console.log(typeof isFeepaid);
+var a = 6 ;var b =4;var c = a+b;alert(a);alert(b);alert(c);
